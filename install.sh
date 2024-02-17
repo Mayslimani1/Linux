6f3c3265127a0bbcd85e6489c0b8031243bd323a
johannaroll <150137066+johannaroll@users.noreply.github.com>
2024-02-17 21:20:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,9 @@ virtualenv venv
 
 source ./venv/bin/activate
 
-#Installer kaggle
-pip install kaggle
-
-# Installer les dépendances Python via pip
-pip install -r requirements.txt
-
-# Installation de Streamlit
-pip install streamlit
+# Installer les dépendances
+pip install --upgrade pip
+pip install kaggle pandas base64 streamlit -r requirements.txt
 
 # Assurez-vous que votre script est exécutable
 chmod +x install.sh
